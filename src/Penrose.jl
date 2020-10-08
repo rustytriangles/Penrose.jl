3ab3eb26383e6b9ddae4f92d53dd67f6deb64869
Mike Garrity <mike@rustytriangles.com>
2020-10-08 14:47:54 -0400

    Basic dart/kite methods

--- src/Penrose.jl
+++ src/Penrose.jl
@@ -1,5 +1,152 @@
 module Penrose
 
-# Write your package code here.
+using GeometryTypes
+
+export Dart, Kite
+export translate, rotate, points, edge_points, edge_center, edge_angle, geometry
+
+struct Dart
+  cx::Number
+  cy::Number
+  angle::Number
+  Dart(x,y,a) = new(x,y,(a+360)%360)
+end
+
+struct Kite
+  cx::Number
+  cy::Number
+  angle::Number
+  Kite(x,y,a) = new(x,y,(a+360)%360)
+end
+
+# Create a new object by rotating an existing one
+function rotate(d::Dart, angle::Number)
+    Dart(d.cx,d.cy,d.angle+angle)
+end
+
+function rotate(k::Kite, angle::Number)
+    Kite(k.cx,k.cy,k.angle+angle)
+end
+
+# Create a new object by translating an existing one
+function translate(d::Dart, dx::Number, dy::Number)
+    @info "Create a copy of d translated by $dx, $dy"
+    Dart(d.cx+dx,d.cy+dy,d.angle)
+end
+
+function translate(k::Kite, dx::Number, dy::Number)
+    Kite(k.cx+dx,k.cy+dy,k.angle)
+end
+
+# Return the 4 points of an object in a form suitable for poly!
+function geometry(d::Dart)
+    c = cos(d.angle * pi / 180);
+    s = sin(d.angle * pi / 180);
+    phi = (1+sqrt(5))/2
+    h = sqrt(5+2*sqrt(5))/2
+
+    Point2f0[[d.cx + c*( 0  ) + s*( 0), d.cy - s*( 0  ) + c*( 0)],
+             [d.cx + c*(-1/2) + s*(-h), d.cy - s*(-1/2) + c*(-h)],
+             [d.cx + c*( phi) + s*( 0), d.cy - s*( phi) + c*( 0)],
+             [d.cx + c*(-1/2) + s*( h), d.cy - s*(-1/2) + c*( h)]]
+end
+
+function geometry(k::Kite)
+    c = cos(k.angle * pi / 180);
+    s = sin(k.angle * pi / 180);
+    phi = (1+sqrt(5))/2
+    h = sqrt(5+2*sqrt(5))/2
+
+    Point2f0[[k.cx + c*(-phi) + s*( 0), k.cy - s*(-phi) + c*( 0)],
+             [k.cx + c*( 1/2) + s*(-h), k.cy - s*( 1/2) + c*(-h)],
+             [k.cx + c*( 1  ) + s*( 0), k.cy - s*( 1 ) + c*( 0)],
+             [k.cx + c*( 1/2) + s*( h), k.cy - s*( 1/2) + c*( h)]]
+end
+
+# Return the two end points of the specified edge (1-4)
+function edge_points(d::Dart, i::Integer)
+    if i<1 || i>4
+        throw(DomainError(i,"out of range"))
+    end
+
+    phi = (1+sqrt(5))/2
+    h = sqrt(5+2*sqrt(5))/2
+    pts = [[0,0], [-1/2,-h], [phi,0], [-1/2,h]]
+    si = i
+    ei = (i)%4 + 1
+    sx = pts[si][1]
+    sy = pts[si][2]
+    ex = pts[ei][1]
+    ey = pts[ei][2]
+    c = cos(d.angle * pi / 180);
+    s = sin(d.angle * pi / 180);
+    [[d.cx + c*sx + s*sy, d.cy - s*sx + c*sy],
+     [d.cx + c*ex + s*ey, d.cy - s*ex + c*ey]]
+end
+
+function edge_points(k::Kite, i::Integer)
+    if i<1 || i>4
+        throw(DomainError(i,"out of range"))
+    end
+
+    phi = (1+sqrt(5))/2
+    h = sqrt(5+2*sqrt(5))/2
+    pts = [[-phi,0], [ 1/2,-h], [1,0], [1/2,h]]
+    si = i
+    ei = (i)%4 + 1
+    sx = pts[si][1]
+    sy = pts[si][2]
+    ex = pts[ei][1]
+    ey = pts[ei][2]
+    c = cos(k.angle * pi / 180);
+    s = sin(k.angle * pi / 180);
+    [[k.cx + c*sx + s*sy, k.cy - s*sx + c*sy],
+     [k.cx + c*ex + s*ey, k.cy - s*ex + c*ey]]
+end
+
+# Return the center point of the specified edge
+function edge_center(d::Dart, i::Integer)
+    pts = edge_points(d,i);
+    [(pts[1][1]+pts[2][1])/2, (pts[1][2]+pts[2][2])/2]
+end
+
+function edge_center(k::Kite, i::Integer)
+    pts = edge_points(k,i);
+    [(pts[1][1]+pts[2][1])/2, (pts[1][2]+pts[2][2])/2]
+end
+
+# Return the angle of the specified edge
+function edge_angle(d::Dart, i::Integer)
+    if i<1 || i>4
+        throw(DomainError(i,"out of range"))
+    end
+
+    if i==1
+        (252 + d.angle) % 360
+    elseif i==2
+        ( 36 + d.angle) % 360
+    elseif i==3
+        (144 + d.angle) % 360
+    elseif i==4
+        (288 + d.angle) % 360
+    end
+end
+
+function edge_angle(k::Kite, i::Integer)
+    if i<1 || i>4
+        throw(DomainError(i,"out of range"))
+    end
+
+    if i==1
+        (324 + k.angle) % 360
+    elseif i==2
+        ( 72 + k.angle) % 360
+    elseif i==3
+        (108 + k.angle) % 360
+    elseif i==4
+        (216 + k.angle) % 360
+    end
+
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,216 @@
 using Penrose
 using Test
 
-@testset "Penrose.jl" begin
-    # Write your tests here.
+# Helper functions
+# Returns length of an edge between 2 pts
+edgelen_fcn(p) = sqrt((p[2][1]-p[1][1])*(p[2][1]-p[1][1])+(p[2][2]-p[1][2])*(p[2][2]-p[1][2]))
+# Returns angle (in degrees) of an edge between 2 pts
+angle_fcn(p) = (360 + atan(p[2][2]-p[1][2], p[2][1]-p[1][1]) * 180 / pi) % 360
+# Returns the mid-point of an edge between 2 pts
+midpt_fcn(p) = [(p[2][1]+p[1][1])/2, (p[2][2]+p[1][2])/2]
+# Returns the distance between 2 pts
+dist_fcn(p1,p2) = sqrt((p2[1]-p1[1])*(p2[1]-p1[1]) + (p2[2]-p1[2])*(p2[2]-p1[2]))
+
+@testset "Dart angle" begin
+
+    # Angles outside [0-360] should get fixed in ctor
+    @testset "angle range" begin
+        @test Dart(0,0,-40).angle == 320
+        @test Dart(0,0,400).angle ==  40
+    end
+
+    @testset "angle internal" begin
+        internal_angles = [36, 72, 36, 216]
+        d = Dart(0,0,0)
+        @test (edge_angle(d,1) + (540 - internal_angles[1]))%360 == edge_angle(d,2)
+        @test (edge_angle(d,2) + (540 - internal_angles[2]))%360 == edge_angle(d,3)
+        @test (edge_angle(d,3) + (540 - internal_angles[3]))%360 == edge_angle(d,4)
+        @test (edge_angle(d,4) + (540 - internal_angles[4]))%360 == edge_angle(d,1)
+    end
+
+    @testset "angle 0" begin
+        d0 = Dart(0,0,0)
+        @test edge_angle(d0, 1) == 252
+        @test edge_angle(d0, 2) ==  36
+        @test edge_angle(d0, 3) == 144
+        @test edge_angle(d0, 4) == 288
+    end
+
+    @testset "angle +90" begin
+        d90 = Dart(0,0,90)
+        @test edge_angle(d90, 1) == 342
+        @test edge_angle(d90, 2) == 126
+        @test edge_angle(d90, 3) == 234
+        @test edge_angle(d90, 4) ==  18
+    end
+
+    # -270 should be the same as +90
+    @testset "angle -270" begin
+        m270 = Dart(0,0,-270)
+        @test edge_angle(m270, 1) == 342
+        @test edge_angle(m270, 2) == 126
+        @test edge_angle(m270, 3) == 234
+        @test edge_angle(m270, 4) ==  18
+    end
+
+    @testset "angle +180" begin
+        d180 = Dart(0,0,180)
+        @test edge_angle(d180, 1) ==  72
+        @test edge_angle(d180, 2) == 216
+        @test edge_angle(d180, 3) == 324
+        @test edge_angle(d180, 4) == 108
+    end
+
+    # -180 should be the same as +180
+    @testset "angle -180" begin
+        m180 = Dart(0,0,-180)
+        @test edge_angle(m180, 1) ==  72
+        @test edge_angle(m180, 2) == 216
+        @test edge_angle(m180, 3) == 324
+        @test edge_angle(m180, 4) == 108
+    end
+
+end
+
+@testset "Kite angle" begin
+
+    @testset "angle internal" begin
+        internal_angles = [72, 144, 72, 72]
+        k = Kite(0,0,0)
+        @test (edge_angle(k,1) + (540 - internal_angles[1]))%360 == edge_angle(k,2)
+        @test (edge_angle(k,2) + (540 - internal_angles[2]))%360 == edge_angle(k,3)
+        @test (edge_angle(k,3) + (540 - internal_angles[3]))%360 == edge_angle(k,4)
+        @test (edge_angle(k,4) + (540 - internal_angles[4]))%360 == edge_angle(k,1)
+    end
+
+    @testset "angle 0" begin
+        k0 = Kite(0,0,0)
+        @test edge_angle(k0, 1) == 324
+        @test edge_angle(k0, 2) ==  72
+        @test edge_angle(k0, 3) == 108
+        @test edge_angle(k0, 4) == 216
+    end
+
+    @testset "angle 90" begin
+        k90 = Kite(0,0,90)
+        @test edge_angle(k90, 1) ==  54
+        @test edge_angle(k90, 2) == 162
+        @test edge_angle(k90, 3) == 198
+        @test edge_angle(k90, 4) == 306
+    end
+
+    @testset "angle -270" begin
+        m0 = Kite(0,0,-270)
+        @test edge_angle(m0, 1) ==  54
+        @test edge_angle(m0, 2) == 162
+        @test edge_angle(m0, 3) == 198
+        @test edge_angle(m0, 4) == 306
+    end
+
+    @testset "angle 180" begin
+        k180 = Kite(0,0,180)
+        @test edge_angle(k180, 1) == 144
+        @test edge_angle(k180, 2) == 252
+        @test edge_angle(k180, 3) == 288
+        @test edge_angle(k180, 4) ==  36
+    end
+
+    @testset "angle -180" begin
+        m180 = Kite(0,0,-180)
+        @test edge_angle(m180, 1) == 144
+        @test edge_angle(m180, 2) == 252
+        @test edge_angle(m180, 3) == 288
+        @test edge_angle(m180, 4) ==  36
+    end
+
+end
+
+@testset "Dart edge slopes" begin
+    d = Dart(0,0,0)
+
+    @test angle_fcn(edge_points(d,1)) == edge_angle(d,1)
+    @test angle_fcn(edge_points(d,2)) == edge_angle(d,2)
+    @test angle_fcn(edge_points(d,3)) == edge_angle(d,3)
+    @test angle_fcn(edge_points(d,4)) == edge_angle(d,4)
+end
+
+@testset "Kite edge slopes" begin
+    k = Kite(0,0,0)
+
+    @test angle_fcn(edge_points(k,1)) == edge_angle(k,1)
+    @test angle_fcn(edge_points(k,2)) == edge_angle(k,2)
+    @test angle_fcn(edge_points(k,3)) == edge_angle(k,3)
+    @test angle_fcn(edge_points(k,4)) == edge_angle(k,4)
+end
+
+@testset "Dart edge lengths" begin
+    d = Dart(0,0,0)
+
+    l1 = (1+sqrt(5))/2
+    l2 = 1 + l1
+
+    @test edgelen_fcn(edge_points(d,1)) == l1
+    @test edgelen_fcn(edge_points(d,2)) == l2
+    @test edgelen_fcn(edge_points(d,3)) == l2
+    @test edgelen_fcn(edge_points(d,4)) == l1
+end
+
+@testset "Kite edge lengths" begin
+    k = Kite(0,0,0)
+
+    l1 = (1+sqrt(5))/2
+    l2 = 1 + l1
+
+    @test edgelen_fcn(edge_points(k,1)) == l2
+    @test edgelen_fcn(edge_points(k,2)) == l1
+    @test edgelen_fcn(edge_points(k,3)) == l1
+    @test edgelen_fcn(edge_points(k,4)) == l2
+end
+
+@testset "Dart edge center" begin
+    d = Dart(0,0,0)
+
+    @test dist_fcn(midpt_fcn(edge_points(d,1)), edge_center(d,1)) == 0
+    @test dist_fcn(midpt_fcn(edge_points(d,2)), edge_center(d,2)) == 0
+    @test dist_fcn(midpt_fcn(edge_points(d,3)), edge_center(d,3)) == 0
+    @test dist_fcn(midpt_fcn(edge_points(d,4)), edge_center(d,4)) == 0
+end
+
+@testset "Kite edge center" begin
+    k = Kite(0,0,0)
+
+    @test dist_fcn(midpt_fcn(edge_points(k,1)), edge_center(k,1)) == 0
+    @test dist_fcn(midpt_fcn(edge_points(k,2)), edge_center(k,2)) == 0
+    @test dist_fcn(midpt_fcn(edge_points(k,3)), edge_center(k,3)) == 0
+    @test dist_fcn(midpt_fcn(edge_points(k,4)), edge_center(k,4)) == 0
+end
+
+@testset "Dart geometry" begin
+    d = Dart(0,0,0)
+
+    pts = geometry(d)
+    @test size(pts) == (4,)
+    @test dist_fcn(pts[1], edge_points(d,1)[1]) < 5e-8
+    @test dist_fcn(pts[2], edge_points(d,1)[2]) < 5e-8
+    @test dist_fcn(pts[2], edge_points(d,2)[1]) < 5e-8
+    @test dist_fcn(pts[3], edge_points(d,2)[2]) < 5e-8
+    @test dist_fcn(pts[3], edge_points(d,3)[1]) < 5e-8
+    @test dist_fcn(pts[4], edge_points(d,3)[2]) < 5e-8
+    @test dist_fcn(pts[4], edge_points(d,4)[1]) < 5e-8
+    @test dist_fcn(pts[1], edge_points(d,4)[2]) < 5e-8
+end
+
+@testset "Kite geometry" begin
+    k = Kite(0,0,0)
+
+    pts = geometry(k)
+    @test size(pts) == (4,)
+    @test dist_fcn(pts[1], edge_points(k,1)[1]) < 5e-8
+    @test dist_fcn(pts[2], edge_points(k,1)[2]) < 5e-8
+    @test dist_fcn(pts[2], edge_points(k,2)[1]) < 5e-8
+    @test dist_fcn(pts[3], edge_points(k,2)[2]) < 5e-8
+    @test dist_fcn(pts[3], edge_points(k,3)[1]) < 5e-8
+    @test dist_fcn(pts[4], edge_points(k,3)[2]) < 5e-8
+    @test dist_fcn(pts[4], edge_points(k,4)[1]) < 5e-8
+    @test dist_fcn(pts[1], edge_points(k,4)[2]) < 5e-8
 end
